d33f1940b9548be4ced0ff5d5e4f85a44ae28ed8
Pedro Nascimento <pnascimento@gmail.com>
2011-04-22 08:38:26 -0300

    simple example with routes.

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require("http");
-function start() {
+var url = require("url");
+
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for "+pathname+" received.");
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("Hello World!");
     response.end();
